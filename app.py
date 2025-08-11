4f321b1f89ad5fc0967ccb178c571200f7a664e3
johnysmn <johnysaymon1@gmail.com>
2025-08-11 13:52:44 -0300

    Alterando o tamanho da coluna password_hash

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ class Usuario(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(200), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     imagem_url = db.Column(db.String(500), nullable=True)
     stack = db.Column(db.String(200), nullable=True)
     linkedin_url = db.Column(db.String(200), nullable=True)
